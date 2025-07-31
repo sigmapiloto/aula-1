01717977dc2510de8727f45426dcab11740bbd65
sigmapiloto <lucasnbraz2008@gmail.com>
2025-07-31 19:03:58 -0300

    Update 2D.js

--- Programacao-Aplicativos-main/listaExercicios/2D.js
+++ Programacao-Aplicativos-main/listaExercicios/2D.js
@@ -4,7 +4,7 @@ let idade = Number(prompt("Digite a idade da pessoa: "));
 let sexoPessoa = prompt("Digite o sexo da pessoa: ");
 
 if(sexoPessoa = 'M' && idade >= 18){
-    console.log("Essa pessoa deve se alistar!");
+    console.log("Essa pessoa tem alistamento obrigatorio");
 }else{
-    console.log("Essa pessoa não precisa se alistar!");
+    console.log("Essa pessoa não é obrigada a se alistar!");
 }
