93d936bf2dfe7ef9c781f6eb5def688014208017
sigmapiloto <lucasnbraz2008@gmail.com>
2025-07-31 19:05:09 -0300

    Update 2E.js

--- Programacao-Aplicativos-main/listaExercicios/2E.js
+++ Programacao-Aplicativos-main/listaExercicios/2E.js
@@ -2,20 +2,20 @@ const prompt = require("prompt-sync")();
 
 let numeros = new Array(5);
 
-numeros[0] = Number(prompt("Digite o número1: "));
-numeros[1] = Number(prompt("Digite o número2: "));
-numeros[2] = Number(prompt("Digite o número3: "));
-numeros[3] = Number(prompt("Digite o número4: "));
-numeros[4] = Number(prompt("Digite o número5: "));
+numeros[0] = Number(prompt("Digite o numero1: "));
+numeros[1] = Number(prompt("Digite o n2: "));
+numeros[2] = Number(prompt("Digite o n3: "));
+numeros[3] = Number(prompt("Digite o n4: "));
+numeros[4] = Number(prompt("Digite o n5: "));
 
-let maiorNumero = numeros[0];
+let maiornumero = numeros[0];
 
 for(let i = 1; i < 5; i++){
-    if(numeros[i] > maiorNumero){
-        maiorNumero = numeros[i];
+    if(numeros[i] > maiornumero){
+        maiornumero = numeros[i];
     }
 }
 
-console.log(`O maior número entre os digitados foi o: `,maiorNumero)
+console.log(`O maior número é: `,maiorNumero)
 
 
